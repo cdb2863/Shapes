ab9bb3f0669f4d7d76dba951adb9797b4ac082f2
Chris Beliveau <cdb2863@rit.edu>
2020-12-07 22:20:21 -0500

    refactor Rectangle

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Rectangle.h"
 Rectangle::Rectangle(int base, int height) {
     this->base = base;
@@ -7,6 +8,9 @@ Rectangle::Rectangle(int base, int height) {
 Rectangle::~Rectangle() {
 
 }
+
+int Rectangle::getBase() { return this->base; }
+int Rectangle::getHeight() {return this->height; }
 
 int Rectangle::getPerimeter() {
     return (2 * this->base) + (2 * this->height);
@@ -25,5 +29,16 @@ bool Rectangle::isSquare() {
     }
 }
 
-int Rectangle::getBase() { return this->base; }
-int Rectangle::getHeight() {return this->height; }
+void Rectangle::printInfo() {
+    std::cout << "This Rectangle's base is " << this->base << std::endl;
+    std::cout << "This Rectangle's height is " << this->height << std::endl;
+    std::cout << "This Rectangle's perimeter is " << this->getPerimeter() << "." << std::endl;
+    std::cout << "This Rectangle's area is " << this->getArea() << "." << std::endl;
+    if(this->isSquare()) {
+        std::cout << "This Rectangle is a square." << std::endl;
+    }
+    else {
+        std::cout << "This Rectangle is not a square." << std::endl;
+    }
+    std::cout << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -5,25 +5,11 @@
 using namespace std;
 
 // Prototypes
-void testRectangle(Rectangle);
 void rectangleVector(int);
 
 int main() {
+    rectangleVector(5);
     return 0;
-}
-
-void testRectangle(Rectangle r) {
-    cout << "This Rectangle's base is " << r.getBase() << endl;
-    cout << "This Rectangle's height is " << r.getHeight() << endl;
-    cout << "This Rectangle's perimeter is " << r.getPerimeter() << "." << endl;
-    cout << "This Rectangle's area is " << r.getArea() << "." << endl;
-    if(r.isSquare()) {
-        cout << "This Rectangle is a square." << endl;
-    }
-    else {
-        cout << "This Rectangle is not a square." << endl;
-    }
-    cout << endl;
 }
 
 void rectangleVector(int nIterations) {
@@ -34,6 +20,6 @@ void rectangleVector(int nIterations) {
     }
 
     for(auto i = rVector.begin(); i != rVector.end(); i++) {
-        testRectangle(*i);
+        (*i).printInfo();
     }
 }
